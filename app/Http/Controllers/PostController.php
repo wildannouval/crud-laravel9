e77ca212e87164357a3b899e367572da275f9301
wildannouval <senangkucing@gmail.com>
2023-02-19 15:35:30 +0700

    Menambahakan method create dan store

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,11 +7,43 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         //get posts
         $posts = Post::latest()->paginate(5);
 
         //render view with posts
         return view('posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        //render view halaman insert form
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        //Memproses data yang dikirimkan dari halaman insert form
+
+        //validate form
+        $this->validate($request,[
+            'image' => 'require|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'require|min:5',
+            'content' => 'require|min:10'
+        ]);
+
+        //upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content
+        ]);
+
+        //redirect to index
+        return redirect()->route('posts.index')->with(['success' => 'Data Berhasil Disimpan!']);
     }
 }
